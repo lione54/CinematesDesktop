0e6c23bff102dab7e3ab32b780531469eaa94c66
lione54 <78593696+lione54@users.noreply.github.com>
2021-06-17 16:26:54 +0200

    Commit 17/06/21 16:26

--- src/main/java/RetrofitClient/RetrofitClientDBInterno.java
+++ src/main/java/RetrofitClient/RetrofitClientDBInterno.java
@@ -1,10 +1,11 @@
 package RetrofitClient;
 
+import org.example.BuildConfig;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientDBInterno {
-    private static final String urlBase = "http://192.168.1.9/cinematesdb/";
+    private static final String urlBase = "http://" + BuildConfig.IP_AWS + "/cinematesdb/";
     private static Retrofit retrofit = null;
     public static Retrofit getClient(){
         if(retrofit == null){
